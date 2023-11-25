f24b1183a3efc8097993ff3a34a11992ffd2e449
Mehar <mehar.fatma202@gmail.com>
2023-11-26 01:16:25 +0530

    added scroll-to-top-button

--- js/script.js
+++ js/script.js
@@ -37,3 +37,26 @@ accordionItems.forEach((item) => {
         }
     });
 });
+
+// Scroll-To-Top-Button
+const mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+const scrollFunction = () => {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.classList.remove("hidden");
+    } else {
+        mybutton.classList.add("hidden");
+    }
+};
+const backToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+window.addEventListener("scroll", scrollFunction);
